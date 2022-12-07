e1565b545cfbed8b362b8f1018054e6cbc792029
Dmitriy Smirnov <git@d-sm.ru>
2022-12-07 23:09:05 +0300

    task2-completed

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import os
 
-filename = 'recipes.txt'
+def read_cook_book(file_path):
+    result = dict()
+    with open(file_path) as file:
+        for line in file:
+            dish_name = line.strip()
+            result[dish_name] = []
+            ingr_amount = int(file.readline().strip())
+            for i in range(ingr_amount):
+                ingr_info = file.readline().strip().split(' | ')
+                result[dish_name].append(
+                    {
+                        'ingredient_name': ingr_info[0],
+                        'quantity': int(ingr_info[1]),
+                        'measure': ingr_info[2]
+                    }
+                )
+            file.readline()
+    return result
 
-cook_book = dict()
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
+    result = dict()
+    for dish_name in dishes:
+        if not dish_name in cook_book.keys():
+            print(f'Ошибка: блюдо {dish_name} отсутствует в книге рецепетов.')
+            return
+        for ingr_dict in cook_book[dish_name]:
+            ingr_name = ingr_dict['ingredient_name']
+            ingr_quantity = ingr_dict['quantity'] * person_count
+            if ingr_name in result.keys():
+                result[ingr_name]['quantity'] += ingr_quantity
+            else:
+                result[ingr_name] = ingr_dict
+                result[ingr_name].pop('ingredient_name')
+                result[ingr_name]['quantity'] = ingr_quantity
+    return result
 
-path = os.path.join(os.path.dirname(__file__), filename)
-with open(path) as file:
-    for line in file:
-        dish_name = line.strip()
-        cook_book[dish_name] = []
-        ingr_amount = int(file.readline().strip())
-        for i in range(ingr_amount):
-            ingr_info = file.readline().strip().split(' | ')
-            cook_book[dish_name].append(
-                {
-                    'ingredient_name': ingr_info[0],
-                    'quantity': ingr_info[1],
-                    'measure': ingr_info[2]
-                }
-            )
-        file.readline()
+def main():
+    file_name = 'recipes.txt'
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
 
-print(cook_book)
+    cook_book = read_cook_book(file_path)
+
+    shop_list = get_shop_list_by_dishes(cook_book,
+        ['Запеченный картофель', 'Омлет'], 2)
+
+    print(shop_list)
+
+main()
 
